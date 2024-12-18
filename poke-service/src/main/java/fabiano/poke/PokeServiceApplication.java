07dbe350dcca39cdd6f4d2054fe9e500adaac816
fabiano de sousa lima <fabianosl@ufrrj.br>
2024-12-18 18:22:49 -0300

    init

---
+++ poke-service/src/main/java/fabiano/poke/PokeServiceApplication.java
@@ -0,0 +1,13 @@
+package fabiano.poke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeServiceApplication.class, args);
+    }
+
+}
